22a9bdbbcb5ec59a205f55ce22b1d537e346cd81
Brian Musembi <brian.musembi@mths.ca>
2021-05-27 22:07:30 +0000

    Added sphere_surface_area.py

--- sphere_surface_area.py
+++ sphere_surface_area.py
@@ -45,7 +45,8 @@ def main():
                 print("")
         except Exception:
             # output
-            print("{0} is not a number! Please try again.".format(radius_input))
+            print("{0} is not a positive integer!"
+                  " Please try again.".format(radius_input))
 
 
 if __name__ == "__main__":
